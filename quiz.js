f00b1dda166a2faa971c9bc39893abbc577236ef
VamshiKrishna-Peddi <100897059+VamshiKrishna-Peddi@users.noreply.github.com>
2022-05-28 11:25:30 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,131 @@
+function truee1(){
+    document.getElementById("disp1").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp1").style.color="#ADFF2F";
+
+}
+
+
+function falsee1(){
+    document.getElementById("disp1").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp1").style.color="#8B0000";
+}
+
+
+function truee2(){
+    document.getElementById("disp2").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp2").style.color="#ADFF2F";
+
+}
+
+
+function falsee2(){
+    document.getElementById("disp2").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp2").style.color="#8B0000";
+}
+
+
+function truee3(){
+    document.getElementById("disp3").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp3").style.color="#ADFF2F";
+
+}
+
+
+function falsee3(){
+    document.getElementById("disp3").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp3").style.color="#8B0000";
+
+}
+
+
+function truee4(){
+    document.getElementById("disp4").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp4").style.color="#ADFF2F";
+
+}
+
+
+function falsee4(){
+    document.getElementById("disp4").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp4").style.color="#8B0000";
+}
+
+
+function truee5(){
+    document.getElementById("disp5").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp5").style.color="#ADFF2F";
+
+}
+
+
+function falsee5(){
+    document.getElementById("disp5").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp5").style.color="#8B0000";
+}
+
+function truee6(){
+    document.getElementById("disp6").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp6").style.color="#ADFF2F";
+
+}
+
+
+function falsee6(){
+    document.getElementById("disp6").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp6").style.color="#8B0000";
+}
+function truee7(){
+    document.getElementById("disp7").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp7").style.color="#ADFF2F";
+
+}
+
+
+function falsee7(){
+    document.getElementById("disp7").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp7").style.color="#8B0000";
+}
+
+function truee8(){
+    document.getElementById("disp8").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp8").style.color="#ADFF2F";
+}
+
+
+function falsee8(){
+    document.getElementById("disp8").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp8").style.color="#8B0000";
+}
+function truee9(){
+    document.getElementById("disp9").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp9").style.color="#ADFF2F";
+
+}
+
+
+function falsee9(){
+    document.getElementById("disp9").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp9").style.color="#8B0000";
+}
+
+function truee10(){
+    document.getElementById("disp10").innerText="WOW... IT'S CORRECT";
+    document.getElementById("disp10").style.color="#ADFF2F";
+}
+
+
+function falsee10(){
+    document.getElementById("disp10").innerText="OOPS..... IT'S WRONG";
+    document.getElementById("disp10").style.color="#8B0000";
+}
+
+
+
+
+
+
+
+
+
+
+
